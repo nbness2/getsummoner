39cb2573b4dfb862585e661d07e4c60821192f01
nbness2 <nbness1337@gmail.com>
2016-04-29 02:05:51 -0600

    Cleanup RWNA
    Clean up, still works. Just looks... better?

--- RWNA.py
+++ RWNA.py
@@ -7,7 +7,7 @@ w = Watcher.w
 
 DIR = 'NBWatch'
 
-regions =  '''
+regions = '''
     North America = 1
     Latin America North = 2
     Latin America South = 3
@@ -34,15 +34,11 @@ regionlist  = {
     }
 
 
-def wait(debug = False, delay = 1):
-    if debug == True:
-        print('wait start')
+def wait_for_request(delay=1):
     while not w.can_make_request():
-        if debug == True:
-            print('wait')
         sleep(delay)
 
-def updateSummoner(summonername, rg = 1, debug  = False):
+def updateSummoner(summonername, rg, summonerid = None):
     if rg == '':
         rg = 1
     try:
@@ -51,54 +47,39 @@ def updateSummoner(summonername, rg = 1, debug  = False):
     except Exception as e:
         print('Region {0} error: {1}'.format(rg, e))
     if not isdir(DIR):
-        if debug == True:
-            print('Created dir: \\'+DIR)
         makedir(DIR)
-    if debug == True:
-        print('check for wait')
-    wait(debug)
+    wait_for_request()
     # Get summoner info
     try:
         summoner = w.get_summoner(name=summonername, region = rg)
-    except Exception as e:
-        if debug == True:
-            print('Summoner name {0}, {2}: {1}'.format(summonername, e, rg))
+    except Exception:
+        print(summonername, rg)
     SID = str(summoner['id'])
-    if debug == True:
-        print('{0}, {1} get_summoner: Pass'.format(summonername,rg))
     if not isdir(DIR+'\\'+SID):
         makedir(DIR+'\\'+SID)
-    wait(debug)
+    wait_for_request()
     with open(DIR+'\\'+SID+'\\summonername.txt', 'w') as f:
         f.write(str(summonername))
-
     # Get summoner's mastery pages
     mastery_pages = w.get_mastery_pages(SID, region = rg)
     with open(DIR+'\\'+SID+'\\masteries.txt', 'w') as f:
         f.write(str(mastery_pages))
-    if debug == True:
-        print('{0}, {1} masteries: Pass'.format(summonername, rg))
-    wait(debug)
+    wait_for_request()
 
     # Get summoner's rune pages
     rune_pages = w.get_rune_pages(SID, rg)
     with open(DIR+'\\'+SID+'\\runes.txt', 'w') as f:
         f.write(str(rune_pages))
-    if debug == True:
-        print('{0}, {1} runes: Pass'.format(summonername, rg))
-    wait(debug)
+    wait_for_request()
 
     # Get summoner's current ranked stats
     try:
         ranked = w.get_ranked_stats(SID, region =  rg)
         with open(DIR+'\\'+SID+'\\ranked.txt', 'w') as f:
             f.write(str(ranked))
-        if debug == True:
-            print('{0}, {1} current season: Pass'.format(summonername, rg))
     except Exception as e:
-        if debug == True:
-            print('{0}, {2} current season: {1}'.format(summonername, e, rg))
-    wait(debug)
+        print('{0}, {2} current season: {1}'.format(summonername, e, rg))
+    wait_for_request()
 
     # Get summoner's season x ranked stats
     for x in range(1,5):
@@ -106,59 +87,37 @@ def updateSummoner(summonername, rg = 1, debug  = False):
             ranked_season_x = w.get_ranked_stats(SID, season = x, region =  rg)
             with open(DIR+'\\'+SID+'\\ranked_s'+str(x)+'.txt', 'w') as f:
                 f.write(str(ranked_season_x))
-            if debug == True:
-                print('{0} Season {1}: Pass'.format(summonername, x))
         except Exception as e:
-            if debug == True:
-                print('{0} Season {1}: {2}'.format(summonername, x, e))
+            print('{0} Season {1}: {2}'.format(summonername, x, e))
 #sn = input('Summoner Name: ')
 #print(regions)
 #rn = input('Input a region number:')
 #updateSummoner(sn, rn, True)
-def update_all_sums(rg = 10, debug = False, delay = 2):
-    if debug == True:
-        print('Updating all sums')
-    a=1905
+def update_all_sums(rg=10, debug=False, delay=2):
+    a=0
     while True:
         try:
             sl=[]
-            if debug == True:
-                print('populating summoner id list')
             for x in range(30):
                 sl.append(str(a))
                 a+=1
-            if debug == True:
-                print('a:',a,'SID list:',sl)
             summonernames = w.get_summoner_name(sl,region=regionlist[int(rg)])
-            if debug == True:
-                print(len(summonernames),summonernames)
             for x,y in summonernames.items():
                 summonername=y
-                if debug == True:
-                    print('summonername:',x)
-                    print('a:',a)
                 rgn = regionlist[int(rg)]
                 DIR = 'NBWatch\\'+rgn
                 if not isdir(DIR):
-                    if debug == True:
-                        print('Created dir: \\'+DIR)
                     makedir(DIR)
-                if debug == True:
-                    print('check for wait')
-                sleep(1)
-                wait(debug, delay)
+                wait_for_request()
                 # Get summoner info
                 try:
                     summoner = w.get_summoner(name=summonername, region = rgn)
-                except Exception as e:
-                    if debug == True:
-                        print('{3}, Summoner name {0}, {2}: {1}'.format(summonername, e, rg, SID))
+                except Exception:
+                    print(rgn, summonername)
                 SID = str(summoner['id'])
-                if debug == True:
-                    print('{2}, {0}, {1} get_summoner: Pass'.format(summonername, rgn, SID))
                 if not isdir(DIR+'\\'+SID):
                     makedir(DIR+'\\'+SID)
-                wait(debug, delay)
+                wait_for_request()
                 with open(DIR+'\\'+SID+'\\summonername.txt', 'w') as f:
                     f.write(str(summonername))
                 # Get summoner's mastery pages
@@ -166,9 +125,7 @@ def update_all_sums(rg = 10, debug = False, delay = 2):
                 mastery_pages = w.get_mastery_pages(SID, region = rgn)
                 with open(DIR+'\\'+SID+'\\masteries.txt', 'w') as f:
                     f.write(str(mastery_pages))
-                if debug == True:
-                    print('{2}, {0}, {1} masteries: Pass'.format(summonername, rgn, SID))
-                wait(debug, delay)
+                wait_for_request()
                 # Get summoner's rune pages
                 sleep(1)
                 rune_pages = w.get_rune_pages(SID, rgn)
@@ -176,7 +133,7 @@ def update_all_sums(rg = 10, debug = False, delay = 2):
                     f.write(str(rune_pages))
                 if debug == True:
                     print('{2}, {0}, {1} runes: Pass'.format(summonername, rgn, SID))
-                wait(debug, delay)
+                wait_for_request()
 
                 # Get summoner's current ranked stats
                 sleep(1)
@@ -184,25 +141,19 @@ def update_all_sums(rg = 10, debug = False, delay = 2):
                     ranked = w.get_ranked_stats(SID, region =  rgn)
                     with open(DIR+'\\'+SID+'\\ranked.txt', 'w') as f:
                         f.write(str(ranked))
-                    if debug == True:
-                        print('{2}, {0}, {1} current season: Pass'.format(summonername, rgn, SID))
                 except Exception as e:
-                    if debug == True:
-                        print('{3}, {0}, {2} current season: {1}'.format(summonername, e, rgn, SID))
+                    print('{3}, {0}, {2} current season: {1}'.format(summonername, e, rgn, SID))
 
                 # Get summoner's season x ranked stats
                 for x in range(1,5):
                     sleep(1)
-                    wait(debug, delay)
+                    wait_for_request()
                     try:
                         ranked_season_x = w.get_ranked_stats(SID, season = x, region =  rgn)
                         with open(DIR+'\\'+SID+'\\ranked_s'+str(x)+'.txt', 'w') as f:
                             f.write(str(ranked_season_x))
-                        if debug == True:
-                            print('{2}, {0} Season {1}: Pass'.format(summonername, x, SID))
                     except Exception as e:
-                        if debug == True:
-                            print('{3}, {0} Season {1}: {2}'.format(summonername, x, e, SID))
+                        print('{3}, {0} Season {1}: {2}'.format(summonername, x, e, SID))
         except Exception as e:
             with open('errors.txt', 'a') as f:
                 f.write('NA, error:')
